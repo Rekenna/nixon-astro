694f060b5a37e0df853a270f01b756dce57bcfca
Ryan McKenna <ryan.mckenna@hey.com>
2024-08-08 10:55:17 -0400

    Instantiate configuration file

---
+++ src/configuration.ts
@@ -0,0 +1,8 @@
+/*
+ ** This file contains the configuration for the site.
+ */
+
+export const configuration = {
+  siteTitle: "Nixon Photography",
+  siteDescription: "Photography by Jake Nixon",
+};
